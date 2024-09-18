f3cae8483857bb08964175865a79ca7227385ebb
lionel_kg <lionel.komguem@inolians.com>
2024-09-19 01:09:00 +0200

    add overlay add review

--- front/src/components/Modal/index.jsx
+++ front/src/components/Modal/index.jsx
@@ -19,34 +19,40 @@ const Index = props => {
   }, []);
 
   return (
-    <div className={`${styles.modal} ${modalStatut && styles.open}`}>
-      <div className={styles.modal_banner}>
-        <div className={styles.modal_content}>
-          <h3 className={styles.modal_title + ' p-2 m-auto'}>
-            {movie.title || movie.original_title}
-          </h3>
-          <textarea
-            id=""
-            cols="30"
-            rows="10"
-            className={styles.form_group}
-            value={content}
-            onChange={e => setContent(e.target.value)}></textarea>
-          <CustomButton
-            label="valider"
-            classes="full-size"
-            onclick={() => {
-              handleFunction();
-            }}
-          />
-        </div>
-      </div>
-      <Button
-        classes={styles.modal_close}
-        onClick={() => setShowModal(false)}
-        text={<LuX />}
+    <>
+      <div
+        className={`${styles.modal_overlay} ${modalStatut && styles.open}`}
       />
-    </div>
+
+      <div className={`${styles.modal} ${modalStatut && styles.open}`}>
+        <div className={styles.modal_banner}>
+          <div className={styles.modal_content}>
+            <h3 className={styles.modal_title + ' p-2 m-auto'}>
+              {movie.title || movie.original_title}
+            </h3>
+            <textarea
+              id=""
+              cols="30"
+              rows="10"
+              className={styles.form_group}
+              value={content}
+              onChange={e => setContent(e.target.value)}></textarea>
+            <CustomButton
+              label="valider"
+              classes="full-size"
+              onclick={() => {
+                handleFunction();
+              }}
+            />
+          </div>
+        </div>
+        <Button
+          classes={styles.modal_close}
+          onClick={() => setShowModal(false)}
+          text={<LuX />}
+        />
+      </div>
+    </>
   );
 };
 
