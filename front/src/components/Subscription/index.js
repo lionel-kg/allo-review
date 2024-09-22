254c01a0b8362ea3572f490a90a8473e3028c435
lionel_kg <lionel.komguem@inolians.com>
2024-09-22 13:26:59 +0200

    remove others subscriptions

--- front/src/components/Subscription/index.js
+++ front/src/components/Subscription/index.js
@@ -12,27 +12,11 @@ const Index = () => {
   // Exemple
   const plans = [
     {
-      name: 'Pack Découverte',
-      price: '0€ / mois',
-      callToAction: 'Essayez gratuitement',
-      priceValue: 'price_1P6YD9ItkzdIGiy5oYtO235j',
-      advantages: ['Avantage 1'],
-      isTrial: true,
-    },
-    {
       name: 'Pack Standard',
       price: '9.99€ / mois',
       priceValue: 'price_1P6YD9ItkzdIGiy5oYtO235j',
       callToAction: "S'abonner",
       advantages: ['Avantage 1', 'Avantage 2'],
-      isTrial: false,
-    },
-    {
-      name: 'Pack Premium',
-      price: '19.99€ / mois',
-      priceValue: 'price_1P6YD9ItkzdIGiy5oYtO235j',
-      callToAction: "S'abonner",
-      advantages: ['Avantage 1', 'Avantage 2', 'Avantage 3'],
       isTrial: false,
     },
   ];
@@ -42,6 +26,7 @@ const Index = () => {
       <h3 className={styles.subscription_title}>
         A more effective way to track progress
       </h3>
+
       <div className={styles.subscription_pricing}>
         {plans.map((plan, index) => (
           <Plan
